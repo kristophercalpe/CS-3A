a8dd7520bab359aceec2b5f76d0660e86ebb19df
kristophercalpe <44131966+kristophercalpe@users.noreply.github.com>
2022-05-20 01:15:33 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [
+    require('tailwindcss-plugins/pagination'),
+  ],
+}
