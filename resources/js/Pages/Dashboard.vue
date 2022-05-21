e132903ee0eab51397655d8588b0502e9acf7377
Carl Ryan Kristopher Calpe <kristopher.calpe12@gmail.com>
2022-05-21 18:38:41 +0800

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <Head title="Test" />
+    <Head title="Dashboard" />
 
     <BreezeAuthenticatedLayout>
         <template #header>
@@ -17,7 +17,7 @@ import { Head } from '@inertiajs/inertia-vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        Test
+                        You're logged in!
                     </div>
                 </div>
             </div>
