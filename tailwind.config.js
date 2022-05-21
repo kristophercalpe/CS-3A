25fda225a6fa0b6ec595340edf41c27ef0fadd9d
kristophercalpe <44131966+kristophercalpe@users.noreply.github.com>
2022-05-21 17:40:20 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
-  },
-  plugins: [
-    require('tailwindcss-plugins/pagination'),
-  ],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,16 +6,22 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-const tailwindcss = require('tailwindcss');
-
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .alias({
+        '@': 'resources/js',
     });
+
+if (mix.inProduction()) {
+    mix.version();
+}
