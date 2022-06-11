5f17ebfcc23cf0d7b2e928eeb0b537c4f78109a9
Kathleen Lita <101811785+KathleenLita@users.noreply.github.com>
2022-06-10 23:04:39 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,57 +1,29 @@
-<?php 
-
-session_start();
-
-error_reporting(0);
-
-if (isset($_SESSION['username'])) {
-    header("Location: welcome");
-}
-
-if (isset($_POST['submit'])) {
-	$email = $_POST['email'];
-	$password = md5($_POST['password']);
-
-	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-	$result = mysqli_query($conn, $sql);
-	if ($result->num_rows > 0) {
-		$row = mysqli_fetch_assoc($result);
-		$_SESSION['username'] = $row['username'];
-		header("Location: welcome.php");
-	} else {
-		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-    	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    	<link rel="stylesheet" type="text/css" href="login.css">
 
 <title>Log in</title>
 </head>
 <body>
 	<div class="logform">
-		<form action="" method="POST" class="login-email">
+			<form action="" method="POST" class="login-email">
 			<p class="login-text" style="font-size: 4rem; font-weight: 800;  font-family: 'Merienda One', cursive;  text-transform: lowercase;">liwanag</p>
 			<div class="input-group">
-				<input type="email" style="font-family:Sansita" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+				<input type="email" style="font-family:Sansita" placeholder="Email" name="email" required>
 			</div>
 			<div class="input-group">
-				<input type="password" style="font-family:Sansita" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="password" style="font-family:Sansita" placeholder="Password" name="password" required>
 			</div>
 			<div class="input-group">
 				<button name="submit" class="btn" style="font-family:Sansita">Log in</button>
 			</div>
-			<p class="login-register-text" style="font-family:'Sansita';">No Account? <a href="register" style="font-family:Sansita">Register an Account.</a>.</p>
+			<p class="login-register-text" style="font-family:'Sansita';">No Account? <a href="register.php" style="font-family:Sansita">Register an Account</a>.</p>
 		</form>
 	</div>
 </body>
