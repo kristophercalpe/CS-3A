691c90ba1108bdb653f573d6d61037b1edb3195b
Kathleen Lita <101811785+KathleenLita@users.noreply.github.com>
2022-06-11 05:51:53 -0700

    Update login.php

--- login.php
+++ login.php
@@ -2,28 +2,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
 
-   	<link rel="stylesheet" type="text/css" href="login.css">
+<link href="login.css" rel="stylesheet">
+
 </head>
+
 <body>
-	<div class="logform">
-			<form action="" method="POST" class="login-email">
-			<p class="login-text" style="font-size: 4rem; font-weight: 800;  font-family: 'Merienda One', cursive;  text-transform: lowercase;">liwanag</p>
-			<div class="input-group">
-				<input type="email" style="font-family:Sansita" placeholder="Email" name="email" required>
-			</div>
-			<div class="input-group">
-				<input type="password" style="font-family:Sansita" placeholder="Password" name="password" required>
-			</div>
-			<div class="input-group">
-				<button name="submit" class="btn" style="font-family:Sansita">Log in</button>
-			</div>
-			<p class="login-register-text" style="font-family:'Sansita';">No Account? <a href="register.php" style="font-family:Sansita">Register an Account</a>.</p>
-		</form>
-	</div>
+<br><br><br>
+  <div class="container">
+    <h1 class="liwanag">liwanag</h1>
+
+    <label for="email"><b></b></label>
+    <input type="text" placeholder="Email" name="email" id="email" required><br>
+
+    <label for="psw"><b></b></label>
+    <input type="password" placeholder="Password" name="psw" id="psw" required><br>
+
+  
+    <button type="submit" class="loginbtn">Login</button>
+  </div>
+  
+  <div class="register">
+    <p>Already have an account? <a href="register.php">Register now.</a></p>
+  </div>
+
 </body>
+
 </html>
