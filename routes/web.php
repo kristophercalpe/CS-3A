61c6f015c39264aafdd54f08dfaccc221621561e
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-13 22:20:00 +0800

    Added route for adding new admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,3 +63,5 @@ Route::get('/view', function () {
 Route::get('/changepassword2', function () {
     return view('changepassword2');
 });
+
+Route::post('/admin-store',[AdminController::class,'store']);
