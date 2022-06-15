f7c436f48c913d5f1a090a16c759bc43120d926c
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-16 07:48:04 +0800

    Create UserProfile.php

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    use HasFactory;
+
+    protected $table = "tbl_user";
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'user_fullname',
+        'user_username',
+        'user_email',
+        'user_password',
+    ];
+}
