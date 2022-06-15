5164ba2386a52bc9b4eef1073e829c4295cf3d34
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-16 07:46:14 +0800

    Create UserProfileFormRequest.php

---
+++ app/Http/Requests/UserProfileFormRequest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserProfileFormRequest extends FormRequest
+{
+    /*
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /*
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'password' =>[
+                'required',
+            ],
+        ];
+        if($this->getMethod() == "POST")
+        {
+            $rules += [
+                'fullname' =>[
+                    'required',
+                    'unique:tbl_user,user_fullname',
+                ],
+                'username' =>[
+                    'required',
+                    'unique:tbl_user,user_username',
+                ],
+                'email' =>[
+                    'required',
+                    'email',
+                    'unique:tbl_user,user_email',
+                ],
+            ];
+        }
+        if($this->getMethod() == "PUT")
+        {
+            $rules += [
+                'fullname' =>[
+                    'required',
+                    Rule::unique('tbl_user', 'user_fullname')->ignore($this->user),
+                ],
+                'username' =>[
+                    'required',
+                    Rule::unique('tbl_user', 'user_username')->ignore($this->user),
+                ],
+                'email' =>[
+                    'required',
+                    'email',
+                    Rule::unique('tbl_user', 'user_email')->ignore($this->user),
+                ],
+            ];
+        }
+
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return[
+            'username.unique' => 'The username has already been taken.',
+            'username.required' => 'The username field is required.',
+        ];
+    }
+}
