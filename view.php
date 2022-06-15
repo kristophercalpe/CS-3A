c8b89ae958636f72cb5a52c767f358888ce39a3f
Daniel Encina <101791109+DanEncina@users.noreply.github.com>
2022-06-15 13:23:16 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
+   
+//Everything works as Intended. QA(Encina)
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
