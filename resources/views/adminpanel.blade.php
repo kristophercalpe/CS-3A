decd1b6d6ae27807d3be23f87b2639c05f709641
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-15 19:46:01 +0800

    Update adminpanel.blade.php

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -5,26 +5,6 @@
     {
         echo "Cannot Connect";
     }
-    
-    if (isset($_POST['addnewadmin']))
-    {
-        foreach($_POST as $key => $value)
-        {
-            $_POST[$key]=mysqli_real_escape_string($con, $value);
-        }
-
-        $query ="INSERT INTO `tbl_admin`(`admin_fullname`, `admin_username`, `admin_password`) VALUES ('$_POST[fullname]', '$_POST[name]', '$_POST[password]')";
-
-        if (mysqli_query($con, $query))
-        {
-            header("location:admin_panel.php?success=added");
-        }
-        else
-        {
-            header("location:admin_panel.php?success=add_failed");
-        }
-    }
-
 
     if(isset($_GET['rem']) && $_GET['rem']>0)
     {
@@ -40,29 +20,6 @@
         else
         {
             header("location: adminpanel?success=remove_failed");
-        }
-    }
-
-    if (isset($_POST['updateAdmin']))
-    {
-        foreach($_POST as $key => $value)
-        {
-            $_POST[$key]=mysqli_real_escape_string($con, $value);
-        }
-
-        $query = "SELECT * FROM `tbl_admin` WHERE `admin_id` = '$_POST[adminID]'";
-        $result=mysqli_query($con, $query);
-        $fetch=mysqli_fetch_assoc($result);
-
-        $update="UPDATE `tbl_admin` SET `admin_fullname`='$_POST[name]', `admin_username`='$_POST[username]' WHERE `admin_id` = '$_POST[adminID]'";
-
-        if(mysqli_query($con, $update))
-        {
-            header("location: admin_panel.php?success=updated");
-        }
-        else
-        {
-            header("location: admin_panel.php?success=update_failed");
         }
     }
 ?>
@@ -176,7 +133,6 @@
         }
     }
 </script>
-
 
 </body>
 </html>
