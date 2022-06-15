712769060c051cc1bf2579685bad3faf77876880
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-16 07:20:01 +0800

    added error message display in user panel

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -26,8 +26,19 @@
             </form>
             </div>
     </div>
+    <div class="container mt-5 p-0">
+         @if($errors->any())
+             <ul class="alert alert-warning">
+                 @foreach($errors->all() as $error)
+                 <li>{{$error}}</li>
+                 @endforeach
+             </ul>
+        @endif
 
-        
+        @if(session('message'))
+            <h5>{{session('message')}}</h5>
+       @endif
+     </div>
     <div class="container mt-5 p-0">
         <table class="table table-hover text-center">
             <thead class="bg-dark text-light">
