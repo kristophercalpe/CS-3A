c9b6ce7f9c04ba98a4d6ad35badb5f7e68c17d02
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-19 07:33:03 +0800

    Create RegisterController.php

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterFormRequest;
+use App\Models\Register;
+use Illuminate\Support\Facades\DB;
+
+class RegisterController extends Controller
+{
+    public function store(RegisterFormRequest $request)
+    {
+        try{
+            $data = $request->validated();
+            $tbl_user = new Register;
+
+            //table name->column name = $request->name of input fields
+            $tbl_user->user_fullname = $request->fullname;
+            $tbl_user->user_username = $request->username;
+            $tbl_user->user_email = $request->email;
+            $tbl_user->user_password = $request->password;
+            $tbl_user->save();
+            return redirect('/home');
+
+        }
+        catch(\Exception $e){
+            return redirect('/register')->with('message', 'Something Went Wrong.'.$e);
+        } 
+    }
+
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+  
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\UserProfile $user
+     * @return \Illuminate\Http\Response
+     */
+    
+}
