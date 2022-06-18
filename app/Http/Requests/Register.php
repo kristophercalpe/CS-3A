05a730f8dd8baa6a864e7170304e8bb4fa9fcbe6
Nidesh Acosta <101880363+NideshAcosta@users.noreply.github.com>
2022-06-19 07:18:35 +0800

    Create Register.php

---
+++ app/Http/Requests/Register.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Register extends Model
+{
+    use HasFactory;
+
+    protected $table = "tbl_user";
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'user_fullname',
+        'user_username',
+        'user_email',
+        'user_password',
+    ];
+}
