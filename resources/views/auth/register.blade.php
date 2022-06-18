0521d7e392769c9fae727a2d2a61de463db5e29a
Liezel Guemo <101855973+GuemoLiezel@users.noreply.github.com>
2022-06-18 02:44:21 -0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,45 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<template>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Sansita&display=swap" rel="stylesheet">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<link href="/css/reg.css" rel="stylesheet">
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+</head>
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+<body>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+  <div class="container">
+    <h1 class="liwanag">liwanag</h1>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+    <label for="fullname"><b></b></label>
+    <input type="text" placeholder="Full Name" name="fullname" id="fullname" required><br>
+    
+    <label for="username"><b></b></label>
+    <input type="text" placeholder="Username" name="username" id="username" required><br>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+    <label for="email" ><b></b></label>
+    <input type="text" placeholder="Email" name="email" id="email" required><br>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+    <label for="psw"><b></b></label>
+    <input type="password" placeholder="Password" name="psw" id="psw" required><br>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
+    <label for="psw-repeat"><b></b></label>
+    <input type="password" placeholder="Confirm Password" name="psw-repeat" id="psw-repeat" required><br>
+  
+    <button type="submit" class="registerbtn">Register</button>
+  </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+  <div class="signin">
+    <p>Already have an account? <a href="login">Login</a></p>
+  </div>
+ 
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
+</body>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
+</template>
